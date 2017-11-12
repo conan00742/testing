6afe780fb919f7078ddefd154c80e3c5595f9e3e
Khiem Nguyen <1359033@itec.hcmus.edu.vn>
2017-11-12 21:08:46 +0700

    upload image commit

---
+++ app/src/main/java/com/example/krot/downloadmanager/demo/NewBranch.java
@@ -0,0 +1,15 @@
+package com.example.krot.downloadmanager.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.krot.downloadmanager.R;
+
+public class NewBranch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_branch);
+    }
+}
